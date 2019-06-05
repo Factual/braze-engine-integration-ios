c3f05410cd8c29c7ca4de7653c8613a706b587e5
Nathan <nnlamb25@gmail.com>
2019-06-05 15:22:23 -0700

    Compatible with FactualEngineSDK 10

--- library/BrazeEngine.h
+++ library/BrazeEngine.h
@@ -12,13 +12,6 @@
 @interface BrazeEngine : NSObject
 
 /**
- Returns true if tracking circumstances is enabled and false if it is not.
- 
- Call trackCircumstancesWithEngine: to start tracking circumstances and trackCircumstancesWithEngine: to stop tracking circumstances
- */
-+ (BOOL)isTrackingCircumstances;
-
-/**
  Returns true if tracking spans is enabled and false if it is not.
  
  Call trackUserJourneySpans to start tracking circumstances and stopTrackingUserJourneySpans to stop tracking circumstances
@@ -26,55 +19,88 @@
 + (BOOL)isTrackingSpans;
 
 /**
- Configures Factual's Engine SDK to send Braze custom events whenever either
- Engine's circumstance or user journey events are triggered. Note that
- this method assumes that both SDKs have already been started.
-
+ Sends a CircumstanceResponse to Braze as a custom event.  This method assumes that Braze has already been started.
+ 
  The following three types of custom events may be sent:
  
  1) Circumstance Triggered Events:
-    Description: Sent when a Engine circumstance is triggered
-    EventName: "engine_" + CIRCUMSTANCE_NAME
-    EventProperties: incidence_id, user_latitude, user_longitude, event_source
+ 
+    - Description: Circumstance
+ 
+    - EventName: "engine_" + CIRCUMSTANCE_NAME
+ 
+    - EventProperties: incidence_id, user_latitude, user_longitude, event_source
  
  2) Circumstance At Place Events:
-    Description: Sent for each place the user may have been at which triggered the circumstance event
-    EventName: "engine_at_" + CIRCUMSTANCE_NAME
-    EventProperties: Same as (1) with the addition of place properties: name, factual_id, latitude, longitude, place_categories, chain
+ 
+    - Description: Sent for each place the user may have been at which triggered the circumstance event
+ 
+    - EventName: "engine_at_" + CIRCUMSTANCE_NAME
+ 
+    - EventProperties: Same as (1) with the addition of place properties: name, factual_id, latitude, longitude, place_categories, chain
  
  3) Circumstance Near Place Events:
-    Description: Sent for each place the user may have been near to - but not at - which triggered the circumstance event
-    EventName: "engine_near_" + CIRCUSMTANCE_NAME
-    EventProperties: Same as (2)
  
- For each circumstance that is triggered there will be one event of type (1) and potentially
- multiple events of type (2) and type (3). Based on the generality of the circumstance definition,
- Factual's Engine SDK may match multiple potential candidate places the user may have been at or
- near when the circumstance was triggered.
+    - Description: Sent for each place the user may have been near to - but not at - which triggered the circumstance event
  
- @param engine FactualEngine given from engineDidStartWithInstance:
- @param maxAtPlaceEventsPerCircumstance max number of AtPlace events to send to Braze. Sent in order of decreasing confidence.
- @param maxNearPlaceEventsPerCircumstance max number of NearPlace events to send to Braze. Sent in order of decreasing confidence.
+    - EventName: "engine_near_" + CIRCUSMTANCE_NAME
+ 
+    - EventProperties: Same as (2)
+ 
+ For each circumstance response sent there will be one event of type (1) and a maximum of 10 events of type (2) and a
+ maximum of 20 events of type (3).  If you wish to send more or less events of type (2) and (3) use the method
+ pushToBraze:withMaxAtPlaceEvents:withMaxNearPlaceEvents.
+ 
+ Based on the generality of the circumstance definition, Factual's Engine SDK may match multiple potential candidate
+ places the user may have been at or near when the circumstance was triggered.
+ 
+ @param circumstanceResponse A CircumstanceResponse given in circumstancesMet: in the FactualEngineDelegate
  */
-+ (void)trackCircumstancesWithEngine:(FactualEngine *)engine
- withMaxAtPlaceEventsPerCircumstance:(int)maxAtPlaceEventsPerCircumstance
-withMaxNearPlaceEventsPerCircumstance:(int)maxNearPlaceEventsPerCircumstance;
++ (void)pushToBraze:(CircumstanceResponse *)circumstanceResponse;
 
 /**
- Configures Factual's Engine SDK to send Braze custom events whenever either
- Engine's circumstance or user journey events are triggered. Note that
- this method assumes that both SDKs have already been started.
+ Sends a CircumstanceResponse to Braze as a custom event.  This method assumes that Braze has already been started.
  
- @param engine FactualEngine given from engineDidStartWithInstance:
-*/
-+ (void)trackCircumstancesWithEngine:(FactualEngine *)engine;
-
-/**
- Disables Iterable Engine's circumstance tracking
+ The following three types of custom events may be sent:
  
- @param engine FactualEngine given from engineDidStartWithInstance:
+ 1) Circumstance Triggered Events:
+ 
+    - Description: Circumstance
+ 
+    - EventName: "engine_" + CIRCUMSTANCE_NAME
+ 
+    - EventProperties: incidence_id, user_latitude, user_longitude, event_source
+ 
+ 2) Circumstance At Place Events:
+ 
+    - Description: Sent for each place the user may have been at which triggered the circumstance event
+ 
+    - EventName: "engine_at_" + CIRCUMSTANCE_NAME
+ 
+    - EventProperties: Same as (1) with the addition of place properties: name, factual_id, latitude, longitude, place_categories, chain
+ 
+ 3) Circumstance Near Place Events:
+ 
+    - Description: Sent for each place the user may have been near to - but not at - which triggered the circumstance event
+ 
+    - EventName: "engine_near_" + CIRCUSMTANCE_NAME
+ 
+    - EventProperties: Same as (2)
+ 
+ For each circumstance response sent there will be one event of type (1) and you set the maximum number of events of
+ type (2) and type (3) to send.
+ 
+ Based on the generality of the circumstance definition,
+ Factual's Engine SDK may match multiple potential candidate places the user may have been at or near when the
+ circumstance was triggered.
+ 
+ @param circumstanceResponse A CircumstanceResponse given in circumstancesMet: in the FactualEngineDelegate
+ @param maxAtPlaceEvents max number of AtPlace events to send to Braze. Sent in order of decreasing confidence
+ @param maxNearPlaceEvents max number of NearPlace events to send to Braze. Sent in order of decreasing confidence
  */
-+ (void)stopTrackCircumstancesWithEngine:(FactualEngine *)engine;
++ (void)pushToBraze:(CircumstanceResponse *)circumstanceResponse
+withMaxAtPlaceEvents:(int)maxAtPlaceEvents
+withMaxNearPlaceEvents:(int)maxNearPlaceEvents;
 
 /**
  Sends User Journey Spans to Braze.
@@ -82,27 +108,33 @@ withMaxNearPlaceEventsPerCircumstance:(int)maxNearPlaceEventsPerCircumstance;
  The following events may be sent:
  
  1) User Journey Span Events:
-    Description: Sent when a User Journey Span occurs
-    EventName: "engine_span_occurred"
-    EventProperties: span_id, event_source, start_time_unavailable, end_time_unavailable, start_timestamp,
+ 
+    - Description: Sent when a User Journey Span occurs
+ 
+    - EventName: "engine_span_occurred"
+ 
+    - EventProperties: span_id, event_source, start_time_unavailable, end_time_unavailable, start_timestamp,
     end_timestamp, duration, is_home, is_work, ingress_latitude, ingress_longitude, country, localities, postcode,
     region
  
  2) User Journey Span Attached Place Events:
-    Description: Sent for each attached place in a User Journey Span
-    EventName: "engine_span_attached_place"
-    EventProperties: span_id, event_source, name, factual_id, latitude, longitude, place_categories, place_chain
+ 
+    - Description: Sent for each attached place in a User Journey Span
+ 
+    - EventName: "engine_span_attached_place"
+ 
+    - EventProperties: span_id, event_source, name, factual_id, latitude, longitude, place_categories, place_chain
  
  For each User Journey Span that occurs there will be one event of type (1) and multiple events of type (2).  Based
  on the generality of the attached place definition, Factual's Engine SDK may match multiple potential candidate
  places the user may gave been at during the User Journey Span.
  
- You must set Factual Engine's User Journey Delegate to IterableEngineUserJourneyHandler using Factual Engine's
- startWithApiKey:delegate:userJourneyDelegate: method in order to send spans to Iterable
+ You must set Factual Engine's User Journey Delegate to BrazeEngineUserJourneyHandler using Factual Engine's
+ startWithApiKey:delegate:userJourneyDelegate: method in order to send spans to Braze
  */
 + (void)trackUserJourneySpans;
 
-/** Disables Iterable Engine's User Journey Span tracking */
+/** Disables Braze Engine's User Journey Span tracking */
 + (void)stopTrackingUserJourneySpans;
 
 @end
--- library/BrazeEngine.m
+++ library/BrazeEngine.m
@@ -6,44 +6,112 @@
  * Copyright © 2019 Factual Inc. All rights reserved.
  */
 
+#import "Appboy_iOS_SDK/AppboyKit.h"
 #import "BrazeEngine.h"
-#import "BrazeEngineActionHandler.h"
+#import "PlaceCategoryMap.h"
+#import "PlaceChainMap.h"
 
 @implementation BrazeEngine
 
-static BOOL trackingCircumstances = false;
-static BOOL trackingSpans = false;
+NSString *CUSTOM_EVENT_ACTION_ID = @"push-to-braze";
+NSString *SOURCE_NAME = @"factual";
 
-+ (BOOL)isTrackingCircumstances {
-  return trackingCircumstances;
-}
+NSString *BRAZE_ENGINE_CIRCUMSTANCE_KEY = @"engine_";
+NSString *INCIDENT_ID_KEY = @"incident_id";
+NSString *USER_LATITUDE_KEY = @"user_latitude";
+NSString *USER_LONGITUDE_KEY = @"user_longitude";
+NSString *EVENT_SOURCE_KEY = @"event_source";
+
+NSString *BRAZE_ENGINE_AT_PLACE_EVENT_KEY = @"engine_at_";
+NSString *BRAZE_ENGINE_NEAR_PLACE_EVENT_KEY = @"engine_near_";
+NSString *PLACE_NAME_KEY = @"name";
+NSString *PLACE_ID_KEY = @"factual_id";
+NSString *PLACE_LATITUDE_KEY = @"latitude";
+NSString *PLACE_LONGITUDE_KEY = @"longitude";
+NSString *PLACE_CATEGORIES_KEY = @"category_labels";
+NSString *PLACE_CHAIN_KEY = @"chain_name";
+
+static BOOL trackingSpans = false;
 
 + (BOOL)isTrackingSpans {
   return trackingSpans;
 }
 
-+ (void)trackCircumstancesWithEngine:(FactualEngine *)engine
- withMaxAtPlaceEventsPerCircumstance:(int)maxAtPlaceEventsPerCircumstance
-withMaxNearPlaceEventsPerCircumstance:(int)maxNearPlaceEventsPerCircumstance {
-  NSLog(@"Enabling Braze -> Engine circumstance logging");
-  
-  [engine registerActionWithId:[BrazeEngineActionHandler customEventActionID]
-                      listener:[[BrazeEngineActionHandler alloc] initWithMaxAtPlaceEventsPerCircumstance:maxAtPlaceEventsPerCircumstance
-                                                                   withMaxNearPlaceEventsPerCircumstance:maxNearPlaceEventsPerCircumstance]];
-  
-  trackingCircumstances = true;
++ (void)pushToBraze:(CircumstanceResponse *)circumstanceResponse {
+  [BrazeEngine pushToBraze:circumstanceResponse withMaxAtPlaceEvents:10 withMaxNearPlaceEvents:20];
 }
 
-+ (void)trackCircumstancesWithEngine:(FactualEngine *)engine {
-  [self trackCircumstancesWithEngine:engine
- withMaxAtPlaceEventsPerCircumstance:10
-withMaxNearPlaceEventsPerCircumstance:20];
++ (void)pushToBraze:(CircumstanceResponse *)circumstanceResponse
+withMaxAtPlaceEvents:(int)maxAtPlaceEvents
+withMaxNearPlaceEvents:(int)maxNearPlaceEvents {
+  // Circumstance information
+  NSString *circumstanceName = [[circumstanceResponse circumstance] name];
+  CLLocation *userLocation = [circumstanceResponse deviceLocation];
+  NSNumber *latitude = [NSNumber numberWithDouble:userLocation.coordinate.latitude];
+  NSNumber *longitude = [NSNumber numberWithDouble:userLocation.coordinate.longitude];
+  NSString *incidentId = [[NSUUID UUID] UUIDString];
+  
+  NSString *eventKey = [BRAZE_ENGINE_CIRCUMSTANCE_KEY stringByAppendingString:circumstanceName];
+  
+  // Populate properties data
+  NSMutableDictionary *properties = [[NSMutableDictionary alloc] init];
+  [properties setValue:incidentId forKey:INCIDENT_ID_KEY];
+  [properties setValue:latitude forKey:USER_LATITUDE_KEY];
+  [properties setValue:longitude forKey:USER_LONGITUDE_KEY];
+  [properties setValue:SOURCE_NAME forKey:EVENT_SOURCE_KEY];
+  
+  // Send to Braze
+  [[Appboy sharedInstance] logCustomEvent:eventKey withProperties:properties];
+  
+  // Send any At Places data
+  if (maxAtPlaceEvents > 0 &&
+      circumstanceResponse.atPlaces &&
+      [circumstanceResponse.atPlaces count] > 0) {
+    NSUInteger maxAtPlaces = MIN(maxAtPlaceEvents, [circumstanceResponse.atPlaces count]);
+    NSString * atPlaceEventKey = [BRAZE_ENGINE_AT_PLACE_EVENT_KEY stringByAppendingString:circumstanceName];
+    [self sendPlacesData:circumstanceResponse.atPlaces
+           withEventName:atPlaceEventKey
+          withProperties:properties
+      withMaxPlaceEvents:maxAtPlaces];
+  }
+  
+  // Send any Near Places data
+  if (maxNearPlaceEvents > 0 &&
+      circumstanceResponse.nearPlaces &&
+      [circumstanceResponse.nearPlaces count] > 0) {
+    NSUInteger maxNearPlaces = MIN(maxNearPlaceEvents, [circumstanceResponse.nearPlaces count]);
+    NSString *nearPlaceEventKey = [BRAZE_ENGINE_NEAR_PLACE_EVENT_KEY stringByAppendingString:circumstanceName];
+    [self sendPlacesData:circumstanceResponse.nearPlaces
+           withEventName:nearPlaceEventKey
+          withProperties:properties
+      withMaxPlaceEvents:maxNearPlaces];
+  }
 }
 
-+ (void)stopTrackCircumstancesWithEngine:(FactualEngine *)engine {
-  NSLog(@"Disabling Braze -> Engine circumstance logging");
-  [engine disableCircumstanceWithId:[BrazeEngineActionHandler customEventActionID]];
-  trackingCircumstances = false;
++ (void)sendPlacesData:(NSArray<FactualPlace *> *)places
+         withEventName:(NSString *)eventName
+        withProperties:(NSMutableDictionary *)properties
+    withMaxPlaceEvents:(NSUInteger)maxPlaceEvents {
+  // Loop through each place up to maxPlaceEvents
+  for (int i = 0; i < maxPlaceEvents; i++) {
+    FactualPlace *place = places[i];
+    // Get categoires
+    NSString *categories = [[PlaceCategoryMap getCategoriesFromPlace:place] componentsJoinedByString:@", "];
+    
+    // Get chain
+    NSString *chain = [PlaceChainMap getChainFromPlace:place];
+    
+    // Populate properties data
+    [properties setValue:place.name forKey:PLACE_NAME_KEY];
+    [properties setValue:place.factualId forKey:PLACE_ID_KEY];
+    [properties setValue:[NSNumber numberWithDouble:place.latitude] forKey:PLACE_LATITUDE_KEY];
+    [properties setValue:[NSNumber numberWithDouble:place.longitude] forKey:PLACE_LONGITUDE_KEY];
+    [properties setValue:categories forKey:PLACE_CATEGORIES_KEY];
+    [properties setValue:chain forKey:PLACE_CHAIN_KEY];
+    
+    // Send to Braze
+    [[Appboy sharedInstance] logCustomEvent:eventName withProperties:properties];
+  }
 }
 
 + (void)trackUserJourneySpans {
